c6e5ba0e3f21f3853ad97b5b57e8b7234e585230
Simon <simon.faksvag@gmail.com>
2024-07-12 11:34:45 +0200

    JS functions and strings

---
+++ mortgage-repayment-calculator-main/main.js
@@ -0,0 +1,73 @@
+const defaultText = document.getElementById('default-text')
+const calculationsContainer = document.getElementById('calculations-container')
+
+document.querySelectorAll('.mortgage-type').forEach(input => {
+    input.addEventListener('change', () => {
+        document.querySelectorAll('.radio-inputs').forEach(div => {
+            div.classList.remove('selected')
+        })
+
+        if (this.checked) {
+            this.parentElement.classList.add('selected')
+        }
+    })
+})
+
+document.getElementById('calculate-btn').addEventListener('click', () => {
+    const amount = parselFloat(document.getElementById('mortgage-amount').value)
+    const term = parselFloat(document.getElementById('mortgage-term').value)
+    const rate = parselFloat(document.getElementById('mortgage-term').value) / 100
+    const mortgagetype = document.querySelector('input[name="mortgage-type"]:checked')
+
+    let isValid = true
+
+    document.querySelectorAll('.form-flex').forEach(el => {
+        el.classList.remove('error')
+    })
+
+    if(isNaN(term) || term <= 0) {
+        document.getElementById('amount-alert').style.display = 'block'
+        document.getElementById('mortgage-amount-main').classList.add('error')
+        isValid = false
+    } else {
+        document.getElementById('amount-alert').style.display = 'none'
+    }
+
+    if(isNaN(amount) || amount <= 0) {
+        document.getElementById('term-alert').style.display = 'block'
+        document.getElementById('mortgage-term-main').classList.add('error')
+        isValid = false
+    } else {
+        document.getElementById('term-alert').style.display = 'none'
+    }
+
+    if(isNaN(rate) || amount <= 0) {
+        document.getElementById('rate-alert').style.display = 'block'
+        document.getElementById('interest-rate-main').classList.add('error')
+        isValid = false
+    } else {
+        document.getElementById('rate-alert').style.display = 'none'
+    }
+
+    if (!mortgagetype) {
+        document.getElementById('type-alert').style.display = 'block'
+        document.querySelector('.radio-inputs').forEach(el => {
+            el.classList.add('error')
+        })
+        isValid = false
+    } else {
+        document.getElementById('type-alert').style.display = 'none'
+        document.querySelector('.radio-inputs').forEach(el => {
+            el.classList.remove('error')
+        })
+    }
+
+    if (isValid) {
+        let monthlyPayment = 0
+        let totalRepayment = 0
+
+        defaultText.classList.add('hide')
+        calculationsContainer.classList.add('show')
+    }
+
+})
